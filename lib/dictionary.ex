83c003a46246ddbae38e3ba1ec323cbcc87f266e
Antonio Britto <antonioabritto@gmail.com>
2018-10-29 23:03:12 -0300

    using "mix format" to format the code

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,10 +1,12 @@
 defmodule Dictionary do
   def word_list do
-    File.read!("assets/words.txt")
+    "assets/words.txt"
+    |> File.read!()
     |> String.split(~r/\n/)
   end
 
   def random_word() do
-    word_list() |> Enum.random()
+    word_list()
+    |> Enum.random()
   end
 end
