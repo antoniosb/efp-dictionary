872a542eab5ed850363f485b632b79fa27daf3c4
Antonio Britto <antonioabritto@gmail.com>
2018-11-01 01:34:49 -0300

    credo suggestions

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,11 +1,14 @@
 defmodule Dictionary do
+  @moduledoc """
+  This module reads the wordlist file and gives us a random word.
+  """
   def word_list do
     "assets/words.txt"
     |> File.read!()
     |> String.split(~r/\n/)
   end
 
-  def random_word() do
+  def random_word do
     word_list()
     |> Enum.random()
   end
