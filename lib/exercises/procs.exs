9d9c07ffa0909cdf9901c06139f4446e976b2c2f
Antonio Britto <antonioabritto@gmail.com>
2018-11-04 13:21:05 -0200

    an example on send and receive

--- lib/exercises/procs.exs
+++ lib/exercises/procs.exs
@@ -1,9 +1,16 @@
 defmodule Procs do
-  def greeter(name) do
-    Process.sleep(1500)
-    IO.puts("Hello #{name}")
+  def greeter() do
+    receive do
+      msg ->
+        IO.puts("Hello #{inspect(msg)}")
+    end
+
+    greeter()
   end
 
-  # used in the 2nd form of spawn
-  # iex> spawn Procs, :greeter, ["world"]
+  # use to receive messages with send:
+  # obs: the recursion un line 8 prevents it from exiting after first call
+  # iex> pid = spawn Procs, :greeter, []
+  # iex> send pid, "world"
+  # Hello "world"
 end
