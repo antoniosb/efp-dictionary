23010bb4dcbdf9ff507198632653823838cff6a6
Antonio Britto <antonioabritto@gmail.com>
2018-11-06 01:34:04 -0200

    building a message chain around nodes

---
+++ lib/exercises/chain.exs
@@ -0,0 +1,31 @@
+defmodule Chain do
+  defstruct(
+    next_node: nil,
+    count: 100_000
+  )
+
+  def start_link(next_node) do
+    spawn_link(Chain, :message_loop, [%Chain{next_node: next_node}])
+    |> Process.register(:chainer)
+  end
+
+  def message_loop(%{count: 0}) do
+    IO.puts("done")
+  end
+
+  def message_loop(state) do
+    receive do
+      {:trigger, list} ->
+        # IO.inspect(list)
+        # :timer.sleep(1000)
+        send({:chainer, state.next_node}, {:trigger, []})
+    end
+
+    message_loop(%{state | count: state.count - 1})
+  end
+end
+
+# # do on all nodes
+# Chain.start_link :"two@Antonios-MacBook-Pro"
+# # then start the chain on the first node
+# send :chainer, {:trigger, []}
